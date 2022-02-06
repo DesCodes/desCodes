b1674f2f084463385758dd3c529915fab40075b3
Desmond <desmond.yan.lai@gmail.com>
2022-02-06 17:09:57 -0500

    test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('#pagepiling').pagepiling({
     menu: '#menu',
-    anchors: ['page1', 'page2', 'page3'],
+    anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
     sectionsColor: ['#bfda00', '#2ebe21', '#2C3E50', '#51bec4'],
     loopTop: true,
     loopBottom: true
