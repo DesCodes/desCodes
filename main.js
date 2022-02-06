b3c734fad47769dbcc8e49a869fdd39cab9e3be3
Desmond <desmond.yan.lai@gmail.com>
2022-02-06 17:56:05 -0500

    test

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-$(document).ready(function() {
-  $('#pagepiling').pagepiling({
-    menu: '#menu',
-    anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
-    sectionsColor: ['#bfda00', '#2ebe21', '#2C3E50', '#51bec4'],
-    loopTop: true,
-    loopBottom: true
-  });
+// $(document).ready(function() {
+//   $('#pagepiling').pagepiling({
+//     menu: '#menu',
+//     anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+//     sectionsColor: ['#bfda00', '#2ebe21', '#2C3E50', '#51bec4'],
+//     loopTop: true,
+//     loopBottom: true
+//   });
+// });
+new PanelSnap({
+	container: document.getElementById('pagepiling'),
+	panelSelector: '> section'
 });
