286ecbaf7e0b132fec926b1d56009f90d46b724c
Desmond <desmond.yan.lai@gmail.com>
2022-02-07 09:47:16 -0500

    added panelsnap

--- main.js
+++ main.js
@@ -32,17 +32,56 @@
 // new PanelSnap(defaultOptions);
 
 
-// document.addEventListener("DOMContentLoaded", function() {
-//   var options = {
-//     container: document.getElementById('pagepiling'),
-//     panelSelector: '.section',
-//     directionThreshold: 100,
-//     delay: 0,
-//     duration: 300,
-//   };
+document.addEventListener("DOMContentLoaded", function() {
+  var options = {
+  	container: document.body,
+    panelSelector: 'section',
+    directionThreshold: 100,
+    delay: 2,
+    duration: 300,
+    easing: function(t) { return t }
+  };
 
-//   new PanelSnap(options);
-// });  
+new PanelSnap(options);
+});  
+
+// var menu, menuItems, panelSnapInstance;
+
+// document.addEventListener("DOMContentLoaded", function () {
+// menu = document.querySelector('#menu');
+// menuItems = menu.querySelectorAll('button');
+
+// panelSnapInstance = new PanelSnap({
+//   panelSelector: "section",
+//   strictContainerSelection: "false",
+//   directionThreshold: 100,
+//   delay: 2,
+//   duration: 300,
+//   easing: function(t) { return t }
+// });
+// panelSnapInstance.on('activatePanel', activateMenuItem);
+
+// menuItems.forEach(function (menuItem) {
+//   menuItem.addEventListener('click', onButtonClick);
+// })
+
+// });
+
+// function activateMenuItem(panel) {
+// menuItems.forEach(function (menuItem) {
+//   menuItem.classList.remove('active');
+// });
+
+// var panelName = panel.getAttribute('data-panel')
+// var menuItem = menu.querySelector('button[data-panel="' + panelName + '"]');
+// menuItem.classList.add('active');
+// }
+
+// function onButtonClick(e) {
+// var panelName = e.target.getAttribute('data-panel')
+// var panel = document.querySelector('section[data-panel="' + panelName + '"]');
+// panelSnapInstance.snapToPanel(panel);
+// }
 
 // side menu function
 $('nav .rightBox svg').click(function() {
